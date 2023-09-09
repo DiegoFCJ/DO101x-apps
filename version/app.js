61e5ebc944a8e2376b8c424ef0653c8132d25fee
diegoFCJ <diegoeffe96@gmail.com>
2023-09-09 21:16:38 +0200

    changed function position should work

--- version/app.js
+++ version/app.js
@@ -9,49 +9,49 @@ app.get('/', function (req, res) {
     mostraPopup();
     //send the response to the client
     res.send(response);
-
+    function mostraPopup() {
+      // Crea un elemento div per il popup
+      const popup = document.createElement("div");
+      popup.classList.add("popup");
+    
+      // Aggiungi il contenuto al popup
+      popup.innerHTML = `
+        <p>Vuoi andare avanti?</p>
+        <button id="si">Si</button>
+        <button id="no">No</button>
+        <button id="chiudi">X</button>
+      `;
+    
+      // Aggiungi il popup al documento
+      document.body.appendChild(popup);
+    
+      // Imposta un gestore di eventi per il pulsante "Si"
+      document.getElementById("si").addEventListener("click", () => {
+        // Cambia il testo e disabilita i pulsanti
+        popup.innerHTML = `
+          <p>Si, ok, sei andato avanti. E ora?</p>
+          <button id="chiudi">X</button>
+        `;
+      });
+    
+      // Imposta un gestore di eventi per il pulsante "No"
+      document.getElementById("no").addEventListener("click", () => {
+        // Chiudi il popup
+        document.body.removeChild(popup);
+      });
+    
+      // Imposta un gestore di eventi per il pulsante "X"
+      document.getElementById("chiudi").addEventListener("click", () => {
+        // Chiudi il popup
+        document.body.removeChild(popup);
+      });
+    }
 });
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
 
-function mostraPopup() {
-  // Crea un elemento div per il popup
-  const popup = document.createElement("div");
-  popup.classList.add("popup");
 
-  // Aggiungi il contenuto al popup
-  popup.innerHTML = `
-    <p>Vuoi andare avanti?</p>
-    <button id="si">Si</button>
-    <button id="no">No</button>
-    <button id="chiudi">X</button>
-  `;
-
-  // Aggiungi il popup al documento
-  document.body.appendChild(popup);
-
-  // Imposta un gestore di eventi per il pulsante "Si"
-  document.getElementById("si").addEventListener("click", () => {
-    // Cambia il testo e disabilita i pulsanti
-    popup.innerHTML = `
-      <p>Si, ok, sei andato avanti. E ora?</p>
-      <button id="chiudi">X</button>
-    `;
-  });
-
-  // Imposta un gestore di eventi per il pulsante "No"
-  document.getElementById("no").addEventListener("click", () => {
-    // Chiudi il popup
-    document.body.removeChild(popup);
-  });
-
-  // Imposta un gestore di eventi per il pulsante "X"
-  document.getElementById("chiudi").addEventListener("click", () => {
-    // Chiudi il popup
-    document.body.removeChild(popup);
-  });
-}
 
 
