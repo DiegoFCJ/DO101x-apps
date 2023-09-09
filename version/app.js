55df9c6ba3314ad0d68054db598a3158a81db918
diegoFCJ <diegoeffe96@gmail.com>
2023-09-09 21:13:48 +0200

    added prank button and changed version text

--- version/app.js
+++ version/app.js
@@ -5,8 +5,8 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'lo so che e\' sabato, \neppure eccoci qui alla sera \nio che faccio il mio primo deploy \ne tu che gay chi legge leggi questo testo, \na ma ancora stai leggendo? \n brv cs s f! sn flc pr t!' + '\n';
-
+    response = 'This is version 3 of the app.' + '\n';
+    mostraPopup();
     //send the response to the client
     res.send(response);
 
@@ -15,3 +15,43 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
+
+function mostraPopup() {
+  // Crea un elemento div per il popup
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  // Aggiungi il contenuto al popup
+  popup.innerHTML = `
+    <p>Vuoi andare avanti?</p>
+    <button id="si">Si</button>
+    <button id="no">No</button>
+    <button id="chiudi">X</button>
+  `;
+
+  // Aggiungi il popup al documento
+  document.body.appendChild(popup);
+
+  // Imposta un gestore di eventi per il pulsante "Si"
+  document.getElementById("si").addEventListener("click", () => {
+    // Cambia il testo e disabilita i pulsanti
+    popup.innerHTML = `
+      <p>Si, ok, sei andato avanti. E ora?</p>
+      <button id="chiudi">X</button>
+    `;
+  });
+
+  // Imposta un gestore di eventi per il pulsante "No"
+  document.getElementById("no").addEventListener("click", () => {
+    // Chiudi il popup
+    document.body.removeChild(popup);
+  });
+
+  // Imposta un gestore di eventi per il pulsante "X"
+  document.getElementById("chiudi").addEventListener("click", () => {
+    // Chiudi il popup
+    document.body.removeChild(popup);
+  });
+}
+
+
